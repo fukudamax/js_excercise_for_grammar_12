5b120d68c2218760a9059bc9f7b48cb1f6095ba9
wc-fukudamax <huminsyou@hotmail.com>
2019-09-06 12:54:54 +0900

    modify

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 /**
  * 課題1: Twitterのタイムラインのような配列を作る
  *   - 変数名はtweetsで配列とする
- *   - tweetsの中には複数のオブジェクトがある
+*   - tweetsの中には複数のオブジェクトがある
  *     - 1個1個のオブジェクトはツイートを表す
  *     - 全部で2個のツイートオブジェクトを作る
  *   - ツイートオブジェクトは以下の情報を持つ
@@ -15,6 +15,37 @@
  *         - text : ツイート内容(文字列。ツイート内容は自由に作って良い。)
  */
 
+const tweets = [
+	{
+		'username':'fukuda',
+		'text':'hello',
+		'replies':[
+			{
+				'username':'fukuda2',
+				'text':'hello2'
+			},
+			{
+				'username':'fukuda3',
+				'text':'hello3'
+			}
+		]
+	},
+
+	{
+		'username':'aya',
+		'text':'yeah!',
+		'replies':[
+			{
+				'username':'aya2',
+				'text':'yeah!2'
+			},
+			{
+				'username':'aya3',
+				'text':'yeah!3'
+			}
+		]
+	}
+]
 
 /**
  * 課題2: 課題1で作ったtweetsの内容を全て表示する
@@ -24,3 +55,11 @@
  *     - replies
  *       - repliesの内容もforEachを使って全て表示する
  */
+
+	tweets.forEach(function(element){
+		console.log(element)
+		element.replies.forEach(function(reply){
+			console.log(reply)
+		})
+	})
+
